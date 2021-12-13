d85c0f00c58171c7a5963c6eec135339f8c923ca
VeneS-Sudo <veryworks9@gmail.com>
2021-12-13 08:56:51 +0300

    Добавьте файлы проекта.

---
+++ Legato beat/ViewLocator.cs
@@ -0,0 +1,32 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Legato_beat.ViewModels;
+using System;
+
+namespace Legato_beat
+{
+    public class ViewLocator : IDataTemplate
+    {
+        public bool SupportsRecycling => false;
+
+        public IControl Build(object data)
+        {
+            var name = data.GetType().FullName!.Replace("ViewModel", "View");
+            var type = Type.GetType(name);
+
+            if (type != null)
+            {
+                return (Control)Activator.CreateInstance(type)!;
+            }
+            else
+            {
+                return new TextBlock { Text = "Not Found: " + name };
+            }
+        }
+
+        public bool Match(object data)
+        {
+            return data is ViewModelBase;
+        }
+    }
+}
