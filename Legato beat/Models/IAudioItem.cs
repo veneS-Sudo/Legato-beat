9a4b351d750107c9b6eb7901bbfc0eb2c59a87ec
VeneS-Sudo <veryworks9@gmail.com>
2021-12-22 23:18:25 +0300

    create interface audio metadata and queue

--- Legato beat/Models/IAudioItem.cs
+++ Legato beat/Models/IAudioItem.cs
@@ -1,7 +1,89 @@
-﻿namespace Legato_beat.Models
+﻿using System;
+
+namespace Legato_beat.Models
 {
     public interface IAudioItem
     {
+        /// <summary>
+        /// The metadata for the Album title.
+        /// </summary>
+        string Album { get; set; }
+
+        /// <summary>
+        /// The metadata for a Bitmap typed value to retrieve the information about the artwork for the Album of the media's original source.
+        /// </summary>
+        object AlbumImage { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the Uri of the artwork for the Album of the media's original source.
+        /// </summary>
+        string AlbumImageUri { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the artist of the media.
+        /// </summary>
+        string Artist { get; set; }
+
+        /// <summary>
+        /// The metadata for a Bitmap typed value to retrieve the information about the artwork for the media.
+        /// </summary>
+        object Image { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about Uri of the artwork for the media.
+        /// </summary>
+        string ImageUri { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the date the media was created or published.
+        /// </summary>
+        DateTime Date { get; set; }
+
+        /// <summary>
+        /// The metadata for a int typed value to retrieve the information about the duration of the media.
+        /// </summary>
+        TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the genre of the media.
+        /// </summary>
+        string Genre { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the Uri of the content.
+        /// </summary>
+        string AudioUri { get; set; }
+
+        /// <summary>
+        /// The metadata for a Rating2 typed value to retrieve the information about the overall rating for the media.
+        /// </summary>
+        object Rating { get; set; }
+
+        /// <summary>
+        /// The metadata for a CharSequence or string typed value to retrieve the information about the title of the media.
+        /// </summary>
+        string Title { get; set; }
+
+        /// <summary>
+        /// The metadata for a int typed value to retrieve the information about the year the media was created or published.
+        /// </summary>
+        int Year { get; set; }
+
+        /// <summary>
+        /// The file extension of the media item
+        /// This may not be available for every item
+        /// </summary>
+        string FileExtension { get; set; }
+
+        /// <summary>
+        /// The name of the media file
+        /// </summary>
+        string FileName { get; set; }
+
+        /// <summary>
+        /// Indicates if the MediaItem is being live streamed
+        /// </summary>
+        bool IsLive { get; set; }
 
     }
 }
---
+++ Legato beat/Models/Queue/Queue.cs
@@ -0,0 +1,151 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Legato_beat.Models.Queue
+{
+    public class Queue : IAudioQueue
+    {
+        private int _currentIndex = 0;
+
+        public Queue()
+        {
+            AudioItems = new ObservableCollection<IAudioItem>();
+        }
+
+        public IAudioItem this[int index] { get => AudioItems[index]; set => AudioItems[index] = value; }
+
+        public ObservableCollection<IAudioItem> AudioItems {get; protected set;}
+
+        public bool HasNext => Count > CurrentIndex + 1;
+
+        public bool HasPrevious => CurrentIndex > 0;
+
+        public IAudioItem? Next 
+        { 
+            get
+            {
+                if(HasNext)
+                    return AudioItems[CurrentIndex+1];
+                return null;
+            }
+        }
+
+        public IAudioItem? Previous 
+        { 
+            get
+            {
+                if (HasPrevious)
+                    return AudioItems[CurrentIndex - 1];
+                return null;
+            }
+        }
+
+        public bool HasCurrent => this.ElementAtOrDefault(CurrentIndex) != null;
+
+        public IAudioItem? Current
+        {
+            get
+            {
+                return this.ElementAtOrDefault(_currentIndex);
+            }
+            set
+            {
+                CurrentIndex = AudioItems.IndexOf(value);
+            }
+
+        }
+
+        public int Count => AudioItems.Count;
+
+        public bool IsReadOnly => false;
+
+        public int CurrentIndex 
+        { 
+            get => _currentIndex;
+            set
+            {
+                _currentIndex = value;
+                OnPropertyChanged("CurrentIndex");
+                if (HasCurrent)
+                {
+                    OnQueueChanged(this, new QueueChangedEventArgs(Current));
+                }
+            }
+        }
+
+        public event QueueEndedEventHandler QueueEnded;
+        public event QueueChangedEventHandler QueueChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnQueueChanged(object sender, QueueChangedEventArgs args)
+        {
+            QueueChanged?.Invoke(sender, args);
+        }
+
+        internal void OnQueueEnded(object sender, QueueEndedEventArgs args)
+        {
+            QueueEnded?.Invoke(sender, args);
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        public void Add(IAudioItem item)
+        {
+            AudioItems.Add(item);
+        }
+
+        public void Clear()
+        {
+            AudioItems.Clear();
+        }
+
+        public bool Contains(IAudioItem item)
+        {
+            return AudioItems.Contains(item);
+        }
+
+        public void CopyTo(IAudioItem[] array, int arrayIndex)
+        {
+            AudioItems.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<IAudioItem> GetEnumerator()
+        {
+            return AudioItems.GetEnumerator();
+        }
+
+        public int IndexOf(IAudioItem item)
+        {
+            return AudioItems.IndexOf(item);   
+        }
+
+        public void Insert(int index, IAudioItem item)
+        {
+            AudioItems.Insert(index, item);
+        }
+
+        public bool Remove(IAudioItem item)
+        {
+            return AudioItems.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            AudioItems.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return AudioItems.GetEnumerator();
+        }
+    }
+}
